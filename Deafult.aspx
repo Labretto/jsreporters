ed3c01d89f01a68e8f9af64657edc5c48035c946
Labretto <Labretto@users.noreply.github.com>
2017-08-08 17:27:07 +0530

    v0.1
    Working example using Jsreport.Local and Jsreport.Binary

--- Deafult.aspx
+++ Deafult.aspx
@@ -9,7 +9,60 @@
 <body>
     <form id="form1" runat="server">
         <div>
-            <asp:Button ID="Button1" runat="server" Text="Generate Report" OnClick="Button1_Click" />
+            <div>
+                <section>
+                    Choose which sections must be included in the Report
+                </section>
+                <section>
+                    <table style="border:thin">
+                        <tr style="border:thin">
+                            <td style="border:thin">Asset Meta Data</td>
+                            <td style="border:thin">
+                                Rule Base
+                            </td>
+                            <td style="border: thin">Rule Review
+                            </td>
+                            <td style="border: thin">Shadow, Redundant and Groupable Rules
+                            </td>
+                            <td style="border: thin">
+                                Unused Objects
+                            </td>
+                            <td style="border: thin">Insecure Rules
+                            </td>
+                            <td style="border: thin">Critical Ports
+                            </td>
+                            <td style="border: thin">Compliance
+                            </td>
+                            <td style="border: thin">Calculated Overall Risk Score
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="border: thin">
+                                <asp:CheckBox runat="server" ID="chbk1" /></td>
+                            <td style="border: thin">
+                                <asp:CheckBox runat="server" ID="CheckBox1" /></td>
+                            <td style="border: thin">
+                                <asp:CheckBox runat="server" ID="CheckBox2" /></td>
+                            <td style="border: thin">
+                                <asp:CheckBox runat="server" ID="CheckBox3" /></td>
+                            <td style="border:thin">
+                                <asp:CheckBox runat="server" ID="CheckBox4" /></td>
+                            <td style="border: thin">
+                                <asp:CheckBox runat="server" ID="CheckBox5" /></td>
+                            <td style="border: thin">
+                                <asp:CheckBox runat="server" ID="CheckBox6" /></td>
+                            <td style="border: thin">
+                                <asp:CheckBox runat="server" ID="CheckBox7" /></td>
+                            <td style="border: thin">
+                                <asp:CheckBox runat="server" ID="CheckBox8" /></td>
+                        </tr>
+                    </table>
+                </section>
+            </div>
+        </div>
+        <br/>
+        <div>
+            <asp:Button ID="Button1" runat="server" Text="Generate Audit Report" OnClick="Button1_Click" />
         </div>
     </form>
 </body>
--- Deafult.aspx.cs
+++ Deafult.aspx.cs
@@ -1,14 +1,17 @@
 ﻿using System;
-using jsreport.Client;
-using jsreport.Embedded;
 using System.Threading.Tasks;
+using System.Data;
+using jsreport.Binary;
+using jsreport.Types;
+using jsreport.Local;
 using System.IO;
-using jsreport.Client.Entities;
 
 namespace Jsreporter
 {
     public partial class Deafult : System.Web.UI.Page
     {
+        public DataTable dt;
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -16,37 +19,70 @@ namespace Jsreporter
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            GenerateJsReportAsync().Wait();
+            //if checkbox checked atleast one then get data
+            int aid = 1;
+            getCheckedData(aid);
+
+            //            GenerateJsReportAsync().Wait();
+            //Task.Run(GenerateJsReportAsync).GetAwaiter().GetResult();
+
+            Task.Run(GenerateLocalJsReport).GetAwaiter().GetResult();
         }
 
-        protected async Task GenerateJsReportAsync()
+        protected void getCheckedData(int assetid)
         {
+            //connect to the database get the data for the selected asset and fill dt.
+        }
+        //protected async Task GenerateJsReportAsync()
+        //{
 
-            EmbeddedReportingServer embeddedServer = new EmbeddedReportingServer(port: 2000);
+        //    //EmbeddedReportingServer embeddedServer = new EmbeddedReportingServer(port: 2000);
 
 
-            //embeddedServer.StartAsync().Wait();
-            await embeddedServer.StartAsync();
+        //    ////embeddedServer.StartAsync().Wait();
+        //    //await embeddedServer.StartAsync();
 
-            Report result = await embeddedServer.ReportingService.RenderAsync(new RenderRequest()
+        //    //Report result = await embeddedServer.ReportingService.RenderAsync(new RenderRequest()
+        //    //{
+        //    //    template = new Template()
+        //    //    {
+        //    //        content = "<h1>Hello from {{greetings}}</h1>",
+        //    //        recipe = "phantom-pdf",
+        //    //        engine = "handlebars"
+        //    //    },
+        //    //    data = new
+        //    //    {
+        //    //        greetings = ".NET embedded jsreport. Simple Report"
+        //    //    }
+        //    //});
+
+        //    //using (var fileStream = File.Create("C:\\temp\\jsreport.pdf"))
+        //    //{
+        //    //    result.Content.CopyTo(fileStream);
+        //    //}
+
+        //}
+
+        protected async Task GenerateLocalJsReport()
+        {
+            var rs = new LocalReporting().UseBinary(JsReportBinary.GetBinary()).AsUtility().Create();
+
+            var report = await rs.RenderAsync(new RenderRequest()
             {
-                template = new Template()
+                Template = new Template()
                 {
-                    content = "<h1>Hello from {{greetings}}</h1>",
-                    recipe = "phantom-pdf",
-                    engine = "handlebars"
-                },
-                data = new
-                {
-                    greetings = ".NET embedded jsreport. Simple Report"
+                    Recipe = Recipe.PhantomPdf,
+                    Engine = Engine.None,
+                    Content = "Hello from pdf"
                 }
             });
 
-            using (var fileStream = File.Create("C:\\temp\\jsreport.pdf"))
+            using (var fileStream = File.Create("D:\\jsrep\\hellojsrep.pdf"))
             {
-                result.Content.CopyTo(fileStream);
+                report.Content.CopyTo(fileStream);
             }
 
+   
         }
     }
 }
